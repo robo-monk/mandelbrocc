e30f5dc8630cabfba5ae64d845071181366ca23c
robo-monk <rrobomonk@gmail.com>
2024-02-16 11:16:12 +0100

    we got colors and shit

--- src/application.c
+++ src/application.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <SDL.h>
 
-void event_loop()
-{
-  
+void event_loop(SDL_Surface *surface) {
+  for (int y = 0; y < surface->h; y++) {
+    for (int x = 0; x < surface->w; x++) {
+      Uint32 color = SDL_MapRGB(surface->format, x % 255, y % 255, (x + y) % 255);
+      int index = (y * surface->w) + x;
+
+      Uint32 *pixels = (Uint32 *)surface->pixels;
+      pixels[index] = color;
+      // (surface->pixels)[index] = color;
+    }
+  }
 }
+
 
 void init_sdl() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -13,7 +22,10 @@ void init_sdl() {
   }
   else
   {
-    SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    int w_width = 640;
+    int w_height = 480;
+    SDL_Window *window = SDL_CreateWindow("Mandelbrocc", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w_width, w_height, SDL_WINDOW_SHOWN);
+
     if (window == NULL)
     {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -26,7 +38,9 @@ void init_sdl() {
             done = SDL_TRUE;
           }
         }
-        event_loop();
+        SDL_Surface* windowSurface = SDL_GetWindowSurface(window);
+        event_loop(windowSurface);
+        SDL_UpdateWindowSurface(window);
       }
       SDL_DestroyWindow(window);
     }
